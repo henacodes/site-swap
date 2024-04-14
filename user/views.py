ac6a9584c83ffe58ba68fcd4933de65d6dd58862
kirakos <henacodes@gmail.com>
2024-04-14 18:12:07 +0300

    reqiurements.txt added

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 # Create your views here.
 
@@ -7,7 +7,9 @@ from .forms import CreateUserForm
 def create_user_view(request):
     if (request.method == "POST"):
         form = CreateUserForm(request.POST)
-        print("foorm is populated")
+        if (form.is_valid):
+            form.save()
+            redirect("user_login")
     else:
         form = CreateUserForm()
         return render(request, "user/create_user.html", { "form":form })
